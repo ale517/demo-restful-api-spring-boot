3b3715d3107e4556652caf60868647094a0175f5
Alejandro Andrade <alejo.andrade.rojas@gmail.com>
2022-01-06 23:58:46 -0300

    initial commit spring boot app - 2022

---
+++ src/main/java/com/alejandro/demorestfulapispringboot/DemoRestfulApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.demorestfulapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestfulApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestfulApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejandro/demorestfulapispringboot/DemoRestfulApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.demorestfulapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestfulApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
